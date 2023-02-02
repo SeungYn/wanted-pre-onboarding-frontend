b62f0726a5b2d0c6cae560642dff516c7bf13c91
SeungYn <kwls0407@gmail.com>
2023-02-02 23:05:10 +0900

    Docs : 리다이렉트 파일 추가

---
+++ .history/src/App_20230202230420.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import './App.css';
+import ServiceProvider from './context/ServiceContext';
+
+function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (location.pathname === '/') navigate('/todo');
+  }, [location, navigate]);
+
+  return (
+    <ServiceProvider>
+      <div className='App'>
+        <Outlet />
+      </div>
+    </ServiceProvider>
+  );
+}
+
+export default App;
